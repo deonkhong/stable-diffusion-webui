29b7ba580a137d647c107401aca2cb58c7bb48f9
Deon Khong <72732410+deonkhong@users.noreply.github.com>
2023-03-31 17:25:26 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON=C:\Users\xxdeo\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--medvram
 
 call webui.bat
